01e8652c63d6e3f6744d29be8ed7c09f87407b6c
uidevperson <uidevthing@gmail.com>
2017-10-11 20:57:20 +0100

    testing

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+/* ...there may be other imports here */
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
-Vue.use(BootstrapVue);
-console.log('test');
+Vue.use(BootstrapVue)
